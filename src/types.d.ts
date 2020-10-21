33008d99f8dde1d7acfad889df260a4c120526c2
dwightjack <marco.solazzi@gmail.com>
2020-10-21 17:21:08 +0900

    fix: update types for vue3 compatibility (WIP)

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,7 +5,8 @@ export { PropType }
 export interface PropOptions<T = any, D = T> {
   type?: PropType<T> | true | null
   required?: boolean
-  default?: D | ((props: Record<string, unknown>) => D) | null | undefined
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  default?: D | DefaultFactory<D> | null | undefined | object
   validator?(value: unknown): boolean
 }
 
@@ -33,14 +34,24 @@ export type NativeType = string | boolean | number | null | undefined | Function
 export type Constructor = new (...args: any[]) => any
 
 // see https://github.com/vuejs/vue-next/blob/22717772dd83b67ffaa6ad9805c6269e184c7e41/packages/runtime-core/src/componentProps.ts#L67
-export type InferType<T> = T extends { type: null | true }
+export type InferType<T> = T extends null
   ? any
-  : T extends ObjectConstructor | { type: ObjectConstructor }
-  ? { [key: string]: any }
+  : T extends {
+      type: null | true
+    }
+  ? any
+  : T extends
+      | ObjectConstructor
+      | {
+          type: ObjectConstructor
+        }
+  ? Record<string, any>
   : T extends Prop<infer V>
   ? V
-  : T extends PropOptions<infer V>
-  ? V
+  : T extends PropOptions<infer V, infer D>
+  ? unknown extends V
+    ? D
+    : V
   : T extends VueTypeDef<infer V>
   ? V
   : T extends VueTypeValidableDef<infer V>
@@ -49,14 +60,20 @@ export type InferType<T> = T extends { type: null | true }
 
 export type ValidatorFunction<T> = (value: T) => boolean
 
-export type DefaultFactory<T> = (() => T) | T
+export type DefaultFactory<T> = (
+  props: Record<string, unknown>,
+) => T | null | undefined
 
-export type DefaultType<T> = T extends NativeType ? T : DefaultFactory<T>
+export type VueTypesDefaultFactory<T> = DefaultFactory<T> | T
+
+export type DefaultType<T> = T extends NativeType
+  ? T
+  : VueTypesDefaultFactory<T>
 
 export interface VueTypeBaseDef<
   T = unknown,
   D = DefaultType<T>,
-  U = T extends NativeType ? T : () => T
+  U = T extends NativeType ? T : (props: Record<string, unknown>) => T
 > extends PropOptions<T> {
   _vueTypes_name: string
   readonly def: (
@@ -85,7 +102,7 @@ export interface VueTypeShape<T>
 export interface VueTypeLooseShape<T>
   extends VueTypeBaseDef<
     T,
-    DefaultFactory<Partial<T & { [key: string]: any }>>,
+    VueTypesDefaultFactory<Partial<T & { [key: string]: any }>>,
     () => Partial<T> & { [key: string]: any }
   > {
   readonly loose: VueTypeLooseShape<T>
